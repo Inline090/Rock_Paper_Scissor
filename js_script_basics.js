0a9275a589df0eb7bde0ef0800b62fe8b7b9570a
Inline090 <navneet.mitblr2023@learner.manipal.edu>
2025-08-25 18:12:58 +0000

    added final functionality

--- js_script_basics.js
+++ js_script_basics.js
@@ -1,5 +1,24 @@
-let abs = "nice to meet you!";
-        function say_hello(name){
-            return `hello ${name}, ${abs}`;
-        }
-    console.log(say_hello("ram"))
+let inpt = prompt("Enter your choice:").toLowerCase();
+console.log("User: " + inpt);
+
+let ran_int = Math.random();
+let machine;
+
+//Machine's result
+if (ran_int < 0.35) machine = "rock";
+else if (ran_int < 0.75) machine = "paper";
+else machine = "scissor";
+
+console.log("Machine: " + machine.charAt(0).toUpperCase() + machine.slice(1) + "!");
+
+if (inpt === machine) {
+  console.log("Equal!");
+} else if (
+  (inpt === "rock" && machine === "scissor") ||
+  (inpt === "paper" && machine === "rock") ||
+  (inpt === "scissor" && machine === "paper")
+) {
+  console.log("You WIN!");
+} else {
+  console.log("You LOSE!");
+}
